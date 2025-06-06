8d523ff34927d89728c16e143988a91a1f1ea365
Michael Perrotte <mike@mikecorp.ca>
2025-06-06 13:48:29 -0700

    feat: update index
    - add logging
    - update job properties; start on execution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-
+import pino from "pino";
 import { CronJob, CronTime } from "cron";
 import lodash from "lodash";
 
@@ -9,6 +9,8 @@ import fetchObject from "./utils/fetchObject.js";
 import groupObjects from "./utils/groupObjects.js";
 import transformAndUpload from "./utils/transformAndUpload.js";
 
+import { createLogger, getLogger } from "./utils/logger.js";
+
 const { differenceBy } = lodash;
 
 const {
@@ -17,10 +19,15 @@ const {
   DO_SPACE_BUCKET,
   DO_SPACE_ENDPOINT,
   DO_SPACE_REGION,
+  LOG_LEVEL = "info",
 } = process.env;
+
+const logger = createLogger({ level: LOG_LEVEL });
 
 async function handler(done) {
   try {
+    const logger = getLogger("handler");
+
     const s3Client = createClient({
       endpoint: DO_SPACE_ENDPOINT,
       region: DO_SPACE_REGION,
@@ -28,14 +35,19 @@ async function handler(done) {
       secretAccessKey: DO_SPACE_SECRET_KEY,
     });
 
+    logger.info("processing started");
+
     const items = await fetchBucketObjects({
       client: s3Client,
       bucket: DO_SPACE_BUCKET,
     });
+    logger.trace({ items }, "fetchBucketObjects:result");
 
     const { minified, images } = groupObjects(items);
+    logger.trace({ minified, images }, "groupObjects:result");
 
     const unminified = differenceBy(images, minified, "Key");
+    logger.debug({ unminified }, "differenceBy:result");
 
     for (const image of unminified) {
       const imageObject = await fetchObject(image.Key, {
@@ -47,19 +59,56 @@ async function handler(done) {
         bucket: DO_SPACE_BUCKET,
       });
     }
-    done();
+    logger.info("processing completed");
+    done({
+      listedImages: items.length,
+      minifiedImages: minified.length,
+      totalImages: images.length,
+      unminifiedImages: unminified.length,
+    });
   } catch (err) {
-    console.error(err);
+    logger.error({ err }, "unexpected error occourred");
+    throw err;
   }
 }
+
+logger.info("job creating");
 
 const job = CronJob.from({
   name: "minify-images",
   cronTime: "0 */4 */1 * *",
   onTick: handler,
-  onComplete: () => {},
-  errorHandler: (err) => {},
+  onComplete: (data) => {
+    const logger = getLogger("onComplete");
+    logger.info({ ...data }, "processed information");
+  },
+  errorHandler: (err) => {
+    const logger = getLogger("errorHandler");
+    logger.error({ err }, "error occurred");
+  },
   start: false,
   waitForCompletion: true,
   timeZone: "UTC",
 });
+
+logger.info(
+  {
+    job: {
+      isActive: job.isActive,
+      isCallbackRunning: job.isCallbackRunning,
+    },
+  },
+  "job starting",
+);
+
+job.start();
+
+logger.info(
+  {
+    job: {
+      isActive: job.isActive,
+      isCallbackRunning: job.isCallbackRunning,
+    },
+  },
+  "job started",
+);
